6f2782395eef7204d7587abcd4b9fad8d2a3f62f
PawningPawns <38958140+PawningPawns@users.noreply.github.com>
2018-07-08 15:17:31 -0600

    Create return.py

---
+++ return.py
@@ -0,0 +1,37 @@
+print("Hellow World!")
+
+class Transaction:
+  
+  sales_tax = 0.1
+  
+  def __init__(self, total, discount_rate):
+    self.total = total
+    self.tax = Transaction.sales_tax * total
+    self.discount = disocunt_rate 
+    self.total_discount = int(total - (total * discount))
+    
+    
+  def change_rate(self):
+    self.change_rate = self.total
+    return self.change_rate
+  
+  def output_value(self):
+    self.output = self.total
+    self.discount_output = self.total_discount
+    
+    return self.output
+    return self.discount_output
+  
+  def print_value(self):
+    
+    print('{} tax, {} total, {} discounted total'.format(Transaction.sales_tax, self.output, self.discount_output))
+    
+sale_input = int(input())
+
+sale_discount = int(input))
+
+foo1 = Transaction(sale_input, sale_discount)
+
+foo1.print_value()
+
+print('{} total, {} discount'.format(
